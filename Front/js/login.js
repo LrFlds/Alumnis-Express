fe425e474fbb04eb2919740d8f0cb0764238843c
QuentinFournival <quentinfournival2@gmail.com>
2020-12-07 10:17:14 +0100

    front

---
+++ Front/js/login.js
@@ -0,0 +1,33 @@
+
+
+const btn = document.querySelector('#sub');
+
+btn.addEventListener('click', (e) =>{
+    e.preventDefault()   
+    const mail = document.querySelector('#mail');
+    const pass = document.querySelector('#pass');
+
+    const user = {
+        
+        Email: mail.value,
+        Password : pass.value
+    }
+    const userJson = JSON.stringify(user)
+    const option = {
+        method : "POST",
+        headers : {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        body : userJson
+    }
+    fetch("http://localhost:3000/login", option).then(reponse => {
+        if(!reponse.ok){
+            alert("ton pèèèèèèèèèèèèère");
+        }else{
+            return reponse.json()
+        }
+    }).then(data => {
+       window.location.href = `./annuaire.php#${data.Name}#${data.FirstName}` 
+    })
+})
+
