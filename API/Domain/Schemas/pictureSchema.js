7de133f376ffd57fb29928c940e499c452d5aa3f
LrFlds <64799791+LrFlds@users.noreply.github.com>
2020-12-15 12:02:45 +0100

    cloudinary id

--- API/Domain/Schemas/pictureSchema.js
+++ API/Domain/Schemas/pictureSchema.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PictureSchema = new Schema({
     Picture: { type: String }, 
-   
+    Cloudinary_id: { type: String }
 })
 
 module.exports = PictureSchema;
